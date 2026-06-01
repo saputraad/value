d6d7c065abea80c20e5bf683bcc5b0740dfb08ef
saputraad <149788776+saputraad@users.noreply.github.com>
2026-06-01 23:34:08 +0700

    Update buffett_score.py

--- engines/buffett_score.py
+++ engines/buffett_score.py
@@ -39,7 +39,7 @@ class BuffettScoreAnalyzer:
                 0
             )
         )
-
+    
         predictability = (
             PredictabilityAnalyzer(
                 self.data
@@ -50,7 +50,7 @@ class BuffettScoreAnalyzer:
                 0
             )
         )
-
+    
         cashflow = (
             CashflowQualityAnalyzer(
                 self.data
@@ -61,7 +61,7 @@ class BuffettScoreAnalyzer:
                 0
             )
         )
-
+    
         valuation = (
             self.valuation
             .get(
@@ -70,18 +70,43 @@ class BuffettScoreAnalyzer:
             )
         )
 
-        return round(
+    # ------------------
+    # Predictability Multiplier
+    # ------------------
 
-            quality * 0.40
-            +
-            predictability * 0.30
-            +
-            cashflow * 0.15
-            +
-            valuation * 0.15,
+    if predictability >= 90:
 
-            2
-        )
+        multiplier = 1.00
+
+    elif predictability >= 80:
+
+        multiplier = 0.95
+
+    elif predictability >= 70:
+
+        multiplier = 0.85
+
+    elif predictability >= 60:
+
+        multiplier = 0.70
+
+    else:
+
+        multiplier = 0.50
+
+    base_quality = (
+
+        quality * 0.70
+        +
+        cashflow * 0.20
+        +
+        valuation * 0.10
+    )
+
+    return round(
+        base_quality * multiplier,
+        2
+    )
 
     # =====================
     # RATING
