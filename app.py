960ae90ec1872104a8ce83585e02d5b03cf8ce82
saputraad <149788776+saputraad@users.noreply.github.com>
2026-06-03 15:58:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,6 +78,13 @@ st.subheader("Balance Sheet Rows")
 st.write(
     data["balance_sheet"].index.tolist()
 )
+st.subheader(
+    "Cashflow Rows"
+)
+
+st.write(
+    data["cashflow"].index.tolist()
+)
 st.write(
     data["income_statement"].index.tolist()
 )
