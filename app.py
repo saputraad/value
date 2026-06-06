f2462ef78c325b527e380a8cdfe56e5cc6020175
saputraad <149788776+saputraad@users.noreply.github.com>
2026-06-06 18:15:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 # =====================================
 
 st.set_page_config(
-    page_title="Value Investing Workstation1",
+    page_title="Value Investing Workstation",
     page_icon="📈",
     layout="wide"
 )
