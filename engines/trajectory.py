19a359728a023147d6064260f5f736eff26c0901
saputraad <149788776+saputraad@users.noreply.github.com>
2026-06-08 12:17:25 +0700

    Update trajectory.py

--- engines/trajectory.py
+++ engines/trajectory.py
@@ -255,17 +255,21 @@ class TrajectoryAnalyzer:
     # ==========================
 
     def summary(self):
-        revenue_growth = self.revenue_growth()
-
-        earnings_growth = self.earnings_growth()
-        
-        cfo_growth = self.cfo_growth()
-        
-        print(
-            "DEBUG",
-            revenue_growth,
-            earnings_growth,
-            cfo_growth
+        st.write(
+            {
+                "revenue_growth": revenue_growth,
+                "revenue_score": self.growth_to_score(
+                    revenue_growth
+                ),
+                "earnings_growth": earnings_growth,
+                "earnings_score": self.growth_to_score(
+                    earnings_growth
+                ),
+                "cfo_growth": cfo_growth,
+                "cfo_score": self.growth_to_score(
+                    cfo_growth
+                )
+            }
         )
 
         base_score = self.score()
